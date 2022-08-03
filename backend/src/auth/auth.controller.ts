5a2773ef32f438c0648f3b7ee528eec5c2b27880
Praveenraj Pattar <pattar.praveenraj3537@gmail.com>
2022-08-03 18:01:41 +0530

    ➕: Adding class validator and transform

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,4 +1,9 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDTO } from './dto/auth.dto';
 
@@ -7,16 +12,27 @@ export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  signup(@Body() dto: AuthDTO) {
-    console.log({mail: dto.email});
-    console.log(dto.password);
-    
+  signup(
+    @Body('email') email: string,
+    @Body('password') pass: string,
+  ) {
     console.log({
+      email,
+      typeOfEmail: typeof email,
+      pass,
+      typeOfPassword: typeof pass,
+      
+    });
 
-        email: dto.email,
-        pass: dto.password
-    }
-    );
+    // console.log({mail: dto.email});
+    // console.log(dto.password);
+
+    // console.log({
+
+    //     email: dto.email,
+    //     pass: dto.password
+    // }
+    // );
     return this.authService.signup();
   }
 
